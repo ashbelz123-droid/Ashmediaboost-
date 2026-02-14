b49d84e1039f779431c54f62f968966835a9e3b9
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-14 14:00:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+console.log("SUPABASE_SERVICE_ROLE_KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY ? "SET" : "MISSING");require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
