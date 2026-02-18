17913e2ec91c374b4cf2740c86e631ac438d947c
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-18 11:27:47 +0300

    Update child.js

--- backend/routes/child.js
+++ backend/routes/child.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const pathChild = '../data/child_panels.json';
 
-router.get('/wallet/:id', (req, res) => {
-  res.json({ wallet: 50 });
+router.get('/wallet/:id', (req,res)=>{
+  let childs = [];
+  try{ childs = JSON.parse(fs.readFileSync(pathChild,'utf8')); }
+  catch(err){ childs = []; }
+
+  const panel = childs.find(c => c.id === req.params.id);
+  if(panel) res.json({ wallet: panel.wallet });
+  else res.status(404).json({ message:"Child panel not found" });
 });
 
 module.exports = router;
