b09f0499ffa3504927939e0ee2d4a1153b69cf37
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-18 11:25:50 +0300

    Update wallet.js

--- backend/utils/wallet.js
+++ backend/utils/wallet.js
@@ -1,4 +1,33 @@
-function updateWallet(userId, amount) {
-  return true;
+const fs = require('fs');
+const path = '../data/users.json';
+
+function readUsers() {
+  try {
+    const data = fs.readFileSync(path,'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error('Error reading JSON:', err);
+    return [];
+  }
 }
-module.exports = { updateWallet };
+
+function writeUsers(users) {
+  try {
+    fs.writeFileSync(path, JSON.stringify(users, null, 2));
+  } catch(err) {
+    console.error('Error writing JSON:', err);
+  }
+}
+
+function updateWallet(userId, amount){
+  const users = readUsers();
+  const user = users.find(u => u.id === userId);
+  if(user){
+    user.wallet += amount;
+    writeUsers(users);
+    return true;
+  }
+  return false;
+}
+
+module.exports = { updateWallet, readUsers, writeUsers };
