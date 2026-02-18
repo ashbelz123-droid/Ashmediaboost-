431b189773a82c6b15d900df17ebfb114d489cc8
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-18 11:43:23 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/services', require('./routes/services'));
