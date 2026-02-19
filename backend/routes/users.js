8889c8a11af481ea6595534f49ba8ad33712c6e7
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-19 18:15:37 +0300

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// Dummy users list
+// Dummy users
 let users = [
   { username: 'user1', email: 'user1@example.com' },
   { username: 'user2', email: 'user2@example.com' },
   { username: 'user3', email: 'user3@example.com' },
 ];
 
-// Get all users
 router.get('/', (req, res) => {
   res.json({ users });
 });
