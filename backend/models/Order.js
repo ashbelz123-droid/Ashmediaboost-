5300e38fec3d3c1d4f0a581bb8c94f866779d5e1
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-22 18:50:20 +0300

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const OrderSchema = new mongoose.Schema({
-    userId: String,
-    box: String,
-    price: Number,
-    status: { type: String, default: "Pending" },
-    createdAt: { type: Date, default: Date.now }
+    userId:String,
+    package:String,
+    price:Number,
+    status:{type:String,default:"Pending"},
+    createdAt:{type:Date,default:Date.now}
 });
 
-module.exports = mongoose.model("Order", OrderSchema);
+module.exports = mongoose.model("Order",OrderSchema);
