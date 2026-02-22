380c1b2f983ce870543ae5e1ebfbf613375a2a87
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-22 16:00:12 +0300

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function(req,res,next){
+
+ const token = req.headers.authorization;
+
+ if(!token) return res.status(401).json({message:"Unauthorized"});
+
+ try{
+   jwt.verify(token,process.env.JWT_SECRET);
+   next();
+ }catch{
+   res.status(401).json({message:"Invalid token"});
+ }
+
+}
