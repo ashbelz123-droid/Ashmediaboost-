0e309bfc7a8ab2277857425f09653cce166cf9d0
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-22 16:03:11 +0300

    Update and rename auth.js to admin.js

---
+++ backend/routes/admin.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const Order = require("../models/Order");
+
+router.get("/orders",async(req,res)=>{
+ const orders = await Order.find();
+ res.json({orders});
+});
+
+router.post("/approve/:id",async(req,res)=>{
+ await Order.findByIdAndUpdate(req.params.id,{
+   status:"Completed"
+ });
+ res.json({success:true});
+});
+
+module.exports = router;
