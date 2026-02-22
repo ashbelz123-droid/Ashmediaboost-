f1721c04628c8aca74248fd43757e359c6636579
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-22 18:47:22 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -8,19 +9,36 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname,"../public")));
+
+/* DATABASE */
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log("Mongo Error:", err));
+.then(()=>console.log("✅ Database Connected"))
+.catch(err=>console.log(err));
+
+/* ROUTES */
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/orders", require("./routes/orders"));
 app.use("/api/admin", require("./routes/admin"));
 
-app.get("/", (req,res)=>{
+/* HOME */
+
+app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"../public/index.html"));
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, ()=>console.log("Server running on", PORT));
+/* HEALTH CHECK */
+
+app.get("/health",(req,res)=>{
+    res.send("OK");
+});
+
+/* SERVER START */
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT,()=>{
+    console.log("🚀 AshMediaBoost Running");
+});
