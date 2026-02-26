c1f9fbb38fe9c3efb19b5329af202154fee70c81
ashbelz123-droid <ashbelz123@gmail.com>
2026-02-26 15:58:38 +0300

    Delete backend directory

--- backend/models/Provider.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProviderSchema = new mongoose.Schema({
-    name:String,
-    apiKey:String,
-    status:{type:String,default:"hidden"}
-});
-
-module.exports = mongoose.model("Provider",ProviderSchema);
