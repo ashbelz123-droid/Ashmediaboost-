23d5c7ae56e5dd881979a1d27bb43b9b281ffd18
ashbelz123-droid <ashbelz123@gmail.com>
2026-09-04 14:23:57 -0700

    Configure AshMediaBoost Supabase client

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,7 +1,3 @@
 const ASHMEDIABOOST_SUPABASE_URL = 'https://qwjtndamcivxdgsyzgsg.supabase.co';
-const ASHMEDIABOOST_SUPABASE_ANON_KEY = 'REPLACE_WITH_SUPABASE_PUBLISHABLE_KEY';
-
-window.AshMediaBoostSupabase = {
-  url: ASHMEDIABOOST_SUPABASE_URL,
-  anonKey: ASHMEDIABOOST_SUPABASE_ANON_KEY
-};
+const ASHMEDIABOOST_SUPABASE_ANON_KEY = 'sb_publishable_nGrpDCmsuRMAewqnOjY6xQ_clo8A27a';
+window.AshMediaBoostSupabase = { url: ASHMEDIABOOST_SUPABASE_URL, anonKey: ASHMEDIABOOST_SUPABASE_ANON_KEY };
