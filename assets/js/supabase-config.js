dfec52cfbe0a2a32558cec4f0bda65cc63fded30
ashbelz123-droid <ashbelz123@gmail.com>
2026-09-04 14:31:23 -0700

    Set AshMediaBoost Supabase publishable client configuration

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,3 +1,4 @@
-const ASHMEDIABOOST_SUPABASE_URL = 'https://qwjtndamcivxdgsyzgsg.supabase.co';
-const ASHMEDIABOOST_SUPABASE_ANON_KEY = 'sb_publishable_nGrpDCmsuRMAewqnOjY6xQ_clo8A27a';
-window.AshMediaBoostSupabase = { url: ASHMEDIABOOST_SUPABASE_URL, anonKey: ASHMEDIABOOST_SUPABASE_ANON_KEY };
+window.AshMediaBoostSupabase = Object.freeze({
+  url: 'https://qwjtndamcivxdgsyzgsg.supabase.co',
+  anonKey: 'sb_publishable_nGrpDCmsuRMAewqnOjY6xQ_clo8A27a'
+});
